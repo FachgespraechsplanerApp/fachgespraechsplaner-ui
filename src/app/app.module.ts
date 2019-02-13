aa6b04fca8a003cd99d07ca7989de5c483283501
Marvin Birnbach <marvin@fam-birnbach.de>
2019-02-13 23:13:03 +0100

    started to implement angular routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { SettingsComponent } from './views/settings/settings.component';
+import { AdminComponent } from './views/admin/admin.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    SettingsComponent,
+    AdminComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+      { path: 'login', component: AppComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'settings', component: SettingsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
